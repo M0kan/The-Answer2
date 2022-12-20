cb8921149e51d5938a6b8b88d6bf83a1b21fd79a
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-20 15:49:17 +0000

    hz up

--- docs/components/Body.jsx
+++ docs/components/Body.jsx
@@ -1,3 +1,12 @@
 
 var React = require('react')
-var Header = require('./H
+var Header = require('./Header.jsx')
+var Footer = require('./Footer.jsx')
+var Router = require('react-router')
+var RouteHandler = Router.RouteHandler
+
+var Body = React.createClass({
+
+  render: function () {
+    return (
+      <body class
