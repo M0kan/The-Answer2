272a8e57ac872747525697fbefa9424d75c2ca0a
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-20 17:39:55 +0000

    cleanup build

--- docs/components/ColorList.jsx
+++ docs/components/ColorList.jsx
@@ -34,3 +34,56 @@ var ColorList = React.createClass({
     var ref = 'color-' + i
     var id = color
     var itemId = 'color-item-' + i
+    return (
+      <li ref={ref} key={ref}>
+        <ColorListItem color={color}
+          id={itemId}
+          isEditing={true}
+          removeColor={removeColor}
+          updateColor={updateColor} />
+      </li>
+    )
+  },
+
+  focusInput: function(i) {
+    var li = this.refs['color-' + i].getDOMNode()
+    var input = li.querySelector('input')
+    input.focus()
+    input.select()
+  },
+
+  componentDidUpdate: function() {
+    if (this.state.focus) {
+      this.focusInput(this.state.focus)
+      this.setState({ focus: false })
+    }
+  },
+
+  render: function() {
+    var colors = this.props.colors
+    var styles = {
+      container: {
+        position: 'relative',
+        zIndex: 1,
+        width: '20rem'
+      }
+    }
+    return (
+      <div>
+        <ul className="list-reset mb0"
+          style={styles.container}>
+          {colors.map(this.renderItem)}
+        </ul>
+        <div className="p1 white bg-black">
+          <button className='btn block col-12'
+            onClick={this.addColor}>
+            Add Color
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+})
+
+module.exports = ColorList
---
+++ docs/components/ColorPreview.jsx
@@ -0,0 +1,3 @@
+
+var React = require('react')
+var HeadingBadge = require('./HeadingBadge.jsx')
