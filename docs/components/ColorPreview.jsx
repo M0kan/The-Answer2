c38daf25d40a482e35f3df510e52d95b47670d50
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-20 19:38:25 +0000

    Removed empty lines

--- docs/components/ColorPreview.jsx
+++ docs/components/ColorPreview.jsx
@@ -1,3 +1,19 @@
 
 var React = require('react')
 var HeadingBadge = require('./HeadingBadge.jsx')
+
+var ColorPreview = React.createClass({
+
+  getDefaultProps: function () {
+    return {
+      color: {
+        hex: '',
+      },
+      combo: {
+        hex: '',
+        contrast: 0
+      }
+    }
+  },
+
+  render: function () 
