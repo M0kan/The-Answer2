6cd0ac94675059f6e3c2413b6dede2d64a70c6b8
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-23 11:59:40 +0000

    cifs: adjust sequence number downward after signing NT_CANCEL request

--- docs/components/ColorPreview.jsx
+++ docs/components/ColorPreview.jsx
@@ -34,4 +34,12 @@ var ColorPreview = React.createClass({
           <div className="h2 bold px2">{contrast}</div>
         </div>
         <h2 className="h1 mt0">Contrast</h2>
-        <p className="">Contrast is the difference in luminance or color that makes an object (or its representation in an image or display) distinguishable. In visual perception of the real world, contrast is determined by the difference in the color and brightness of the object and other objects within the same field of view. Because the human visual system is more sensitive to contrast than absolute luminance, we can perceive the world similarly regardless of the huge changes in illumination over the day or from place t
+        <p className="">Contrast is the difference in luminance or color that makes an object (or its representation in an image or display) distinguishable. In visual perception of the real world, contrast is determined by the difference in the color and brightness of the object and other objects within the same field of view. Because the human visual system is more sensitive to contrast than absolute luminance, we can perceive the world similarly regardless of the huge changes in illumination over the day or from place to place. The maximum contrast of an image is the contrast ratio or dynamic range.</p>
+      </div>
+    )
+  }
+
+})
+
+module.exports = ColorPreview
+
