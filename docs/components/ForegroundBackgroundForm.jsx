1da47be540650685853d279aaa2f9bb9b0315d05
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-23 18:49:38 +0000

    Compiler warning fixes

---
+++ docs/components/ForegroundBackgroundForm.jsx
@@ -0,0 +1,8 @@
+
+var React = require('react')
+var Color = require('color')
+var HslSliders = require('react-hsl-sliders')
+
+var ForegroundBackgroundForm = React.createClass({
+
+  handleForegroundChange: functi
