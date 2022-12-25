7a34f205422b8c8cc444823aa22d79fa5efba928
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-25 09:25:18 +0000

    working on machine code compiler

--- docs/components/Header.jsx
+++ docs/components/Header.jsx
@@ -3,4 +3,12 @@ var React = require('react')
 var Ad = require('./Ad.jsx')
 var TweetButton = require('./TweetButton.jsx')
 
-var Header = React.cr
+var Header = React.createClass({
+
+  render: function () {
+    return (
+      <header className='py3'>
+        <div className='sm-flex flex-center mxn2'>
+          <div className='flex-auto px2 py3'>
+            <h1 className='m0'>{this.props.title}</h1>
+       
