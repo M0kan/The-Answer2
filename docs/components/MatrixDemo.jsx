e824ff211d416fb2611fd89f1839fac47be99b36
M0kan <91540734+M0kan@users.noreply.github.com>
2022-12-25 19:41:01 +0000

    Style. Correction code.

--- docs/components/MatrixDemo.jsx
+++ docs/components/MatrixDemo.jsx
@@ -1,3 +1,9 @@
 
 var React = require('react')
-//var qs = require('query-stri
+//var qs = require('query-string')
+//var _debounce = require('lodash').debounce
+var colorable = require('../..')
+var SuperModal = require('./SuperModal.jsx')
+var MatrixRow = require('./MatrixRow.jsx')
+var ColorList = require('./ColorList.jsx')
+var Toolbar = req
