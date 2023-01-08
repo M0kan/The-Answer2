9cc1ba8317b15dac0d0f43d4243eae63507330c0
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-08 09:15:04 +0000

    Fix usage.

--- docs/components/Root.jsx
+++ docs/components/Root.jsx
@@ -4,3 +4,14 @@ var Head = require('./Head.jsx')
 var Nav = require('./Nav.jsx')
 var Footer = require('./Footer.jsx')
 var Router = require('react-router')
+var RouteHandler = Router.RouteHandler
+
+var Root = React.createClass({
+
+  render: function() {
+    var initialProps = {
+      __html: safeStringify(this.props)
+    }
+    return (
+      <html>
+        <Head {...this
