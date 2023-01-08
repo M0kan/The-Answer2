a9e4a40b67d97ddda04847e896595b6535c13ea6
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-08 06:08:08 +0000

    Update the changelog.

--- docs/components/Nav.jsx
+++ docs/components/Nav.jsx
@@ -17,3 +17,47 @@ var Nav = React.createClass({
           Demos
         </a>
         <a href={this.props.baseUrl + 'demos/text'}
+          className='btn py2 sm-show'>
+          Text Demo
+        </a>
+        <a href={this.props.baseUrl + 'demos/matrix'}
+          className='btn py2 sm-show'>
+          Matrix Demo
+        </a>
+
+        {/*
+        <Link to='/colorable/'
+          className='btn py2'>
+          Colorable
+        </Link>
+        <Link to='/colorable/demos/'
+          className='btn py2'>
+          Demos
+        </Link>
+        <Link to='/colorable/demos/text/'
+          activeClassName=''
+          className='btn py2 sm-show'>
+          Text Demo
+        </Link>
+        <Link to='/colorable/demos/matrix/'
+          activeClassName=''
+          className='btn py2 sm-show'>
+          Matrix Demo
+        </Link>
+        */}
+        <div className='flex-auto sm-show' />
+        <a href='https://npmjs.com/package/colorable'
+          className='btn py2'>
+          npm
+        </a>
+        <a href='https://github.com/jxnblk/colorable'
+          className='btn py2'>
+          GitHub
+        </a>
+      </nav>
+    )
+  }
+
+})
+
+module.exports = Nav
---
+++ docs/components/Readme.jsx
@@ -0,0 +1,10 @@
+
+var React = require('react')
+
+var Readme = React.createClass({
+
+  render: function () {
+    return (
+      <div
+        className='prose mb4'
+        dangerouslySetInnerHTML={{ __html: this.
