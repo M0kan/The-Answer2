af6bb1eb61babf20f206453a50e7cddb6209a593
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-08 22:11:07 +0000

    Reduce duplicate code

--- docs/components/ThresholdToggle.jsx
+++ docs/components/ThresholdToggle.jsx
@@ -6,4 +6,13 @@ var ThresholdToggle = React.createClass({
 
   handleChange: function(e) {
     var value = e.target.value
-    this.props.updateThreshold(value
+    this.props.updateThreshold(value)
+  },
+
+  render: function() {
+    var self = this
+    var buttons = [
+      { text: 'None', value: 0 },
+      { text: 'AA Large', value: 3 },
+      { text: 'AA', value: 4.5 },
+      { text: 'AAA'
