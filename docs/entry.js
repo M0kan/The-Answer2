8b95147b29808a0c630a915cf7d3694576ef95eb
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-15 15:43:04 +0000

    Game Update

--- docs/entry.js
+++ docs/entry.js
@@ -7,4 +7,8 @@ if (typeof document !== 'undefined') {
   var initialProps = JSON.parse(document.querySelector('#initial-props').innerHTML)
   Router.run(Routes, Router.HistoryLocation, function (Handler, state) {
     //initialProps.params = state.params
-    React.render(React.createElement(Handler, ini
+    React.render(React.createElement(Handler, initialProps), document)
+  })
+
+  // Tweet button
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platf
