010be70b662ffa3a8daef87da2c640f7097d45f4
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-15 13:58:50 +0000

    let's see

--- docs/entry.js
+++ docs/entry.js
@@ -4,4 +4,7 @@ var Router = require('react-router')
 var Routes = require('./Routes.jsx')
 
 if (typeof document !== 'undefined') {
-  var initialProps = JSON.parse(docum
+  var initialProps = JSON.parse(document.querySelector('#initial-props').innerHTML)
+  Router.run(Routes, Router.HistoryLocation, function (Handler, state) {
+    //initialProps.params = state.params
+    React.render(React.createElement(Handler, ini
