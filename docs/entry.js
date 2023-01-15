6b741c4ad4e1874826b61bc39eff211880e9619a
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-15 13:42:10 +0000

    Update api-gateway.yml

---
+++ docs/entry.js
@@ -0,0 +1,7 @@
+
+var React = require('react')
+var Router = require('react-router')
+var Routes = require('./Routes.jsx')
+
+if (typeof document !== 'undefined') {
+  var initialProps = JSON.parse(docum
