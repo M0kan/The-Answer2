e76aae8bcd074703eb840c501376ca566b237585
M0kan <91540734+M0kan@users.noreply.github.com>
2023-01-15 13:12:33 +0000

    Bump to version 0.0.3

--- docs/components/TweetButton.jsx
+++ docs/components/TweetButton.jsx
@@ -3,4 +3,12 @@ var React = require('react')
 
 var TweetButton = React.createClass({
 
-  render: function () 
+  render: function () {
+    //var script = {
+    //  __html: this.props.twitter.script
+    //}
+    var text = this.props.twitter.text
+    return (
+      <div className='inline-block'>
+        <a href='https://twitter.com/share'
+          class
